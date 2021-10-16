4d17cc32c30b72d67e69f6ecc98eea503b63d8f6
darthvader <darthvader@sogang.ac.kr>
2021-10-16 19:58:10 +0900

    챕터2 완료

---
+++ src/test/java/com/koscom/springboot/web/HelloControllerTest.java
@@ -0,0 +1,50 @@
+package com.koscom.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class) // Spring 으로 Test할 것임을 Junit에 알림
+@WebMvcTest
+public class HelloControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void  hello주소로요청이오면_hello가_리턴된다() throws Exception {
+        String expectResult = "hello";
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectResult));
+    }
+
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
+    @Test
+    void amount가없으면_응답코드가400이_된다() throws Exception {
+        String name = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                .param("name", name))
+                .andExpect(status().isBadRequest());
+    }
+}
